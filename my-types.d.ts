925ee920028a3788d8867284d7e0f0569323c479
cobalthaze <andy.xmoy@Gmailcom>
2021-08-20 14:59:52 -0400

    Working on User.js

--- my-types.d.ts
+++ my-types.d.ts
@@ -10,3 +10,24 @@ export type ErrorRequestHandler = (
 	res: Response,
 	next: NextFunction
 ) => void;
+
+//db.ts
+interface ssl {
+	rejectUnauthorized: boolean;
+}
+interface DialectOptions {
+	ssl: ssl;
+}
+
+export interface Config {
+	logging?: boolean;
+	dialectOptions?: DialectOptions;
+}
+
+// export interface ResponseError extends Error {
+// 	status?: number;
+// }
+
+export interface Error {
+	status?: number;
+}
