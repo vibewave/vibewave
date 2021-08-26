9aaf48b713c08bd41cd4baa1587008d16cebaa87
pdl39 <dhlee9111@gmail.com>
2021-08-26 03:17:17 -0400

    Move the dispatch call to convert user's Spotify auth code to accessToken to the Home component

--- client/components/Home/Home.js
+++ client/components/Home/Home.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AllRooms from '../AllRooms/AllRooms';
+import { useDispatch } from 'react-redux';
+import { spotifyAuthenticate } from '../../store/spotifyAuth';
+
 
 const Home = () => {
+	const dispatch = useDispatch();
+	const authCode = window.localStorage.getItem('spotifyAuthCode');
+
+	useEffect(() => {
+		if (authCode && authCode !== 'authenticated') {
+			dispatch(spotifyAuthenticate(authCode));
+		}
+	}, [authCode]);
+
 	return (
 		<div>
 			<AllRooms />
