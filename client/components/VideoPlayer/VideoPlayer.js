ff620ec40477365c7a6281d446f3e77d8b51a644
Tao Quan <83666521+TDQ2@users.noreply.github.com>
2021-09-02 11:24:35 -0400

    testing multiple users

---
+++ client/components/VideoPlayer/VideoPlayer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactPlayer from 'react-player';
+
+const VideoPlayer = props => {
+	return (
+		<div>
+			<ReactPlayer
+				width="100%"
+				height="29vw"
+				controls
+				url="https://www.youtube.com/watch?v=kTJczUoc26U&ab_channel=TheKidLAROIVEVO"
+			/>
+		</div>
+	);
+};
+
+export default VideoPlayer;
