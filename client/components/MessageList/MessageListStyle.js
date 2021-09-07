fa32e77487a37ff5e2a0a5fd644ed0b0a35ece28
pdl39 <dhlee9111@gmail.com>
2021-09-07 00:43:17 -0400

    Remove comment

--- client/components/MessageList/MessageListStyle.js
+++ client/components/MessageList/MessageListStyle.js
@@ -14,7 +14,6 @@ const useStyles = makeStyles(theme => ({
 		flexDirection: 'column',
 		width: '99%',
 		scrollSnapAlign: 'end',
-		// backgroundColor: theme.palette.secondary.mix,
 		borderRadius: '5px',
 	},
 	bubble: {
